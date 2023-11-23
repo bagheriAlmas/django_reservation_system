9e730d5804ec9894810ffb78a3c9d879e0fdc304
mahdibagheri <bagheri.almas@gmail.com>
2023-11-23 16:34:16 +0330

    Create ListingOwner and Reservation classes

--- src/reservation/models.py
+++ src/reservation/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
 
-# Create your models here.
+
+class ListingOwner(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    listing = models.ForeignKey(ListingOwner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.listing.id} - {self.listing.name} - {self.name}"
+
