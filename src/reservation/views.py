962a8ae23cbcfc3839166f766e42155c4cddfe79
mahdibagheri <bagheri.almas@gmail.com>
2023-11-24 21:24:14 +0330

    Implement paginated endpoint for fetching all listings
    - Introduce 'show_all_listings' API view
    - Utilize PageNumberPagination with a page size of 10
    - Handle exceptions for robust error responses

--- src/reservation/views.py
+++ src/reservation/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Listing
+from .serializers import ListingSerializer
+
+
+@api_view(['GET'])
+def show_all_listings(request):
+    try:
+        paginator = PageNumberPagination()
+        paginator.page_size = 10
+
+        listings = Listing.objects.all()
+        paginated_listings = paginator.paginate_queryset(listings, request)
+
+        serializer = ListingSerializer(paginated_listings, many=True)
+        return paginator.get_paginated_response(serializer.data)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
