06275d36b4c9efaed358d2da8745442f1d007392
mahdibagheri <bagheri.almas@gmail.com>
2023-11-24 21:36:31 +0330

    Implement 'add_reservation' API view with availability check
    - Handles POST requests for adding reservations to listings
    - Validates input dates and returns a 400 Bad Request response on errors
    - Checks listing availability using 'is_listing_available_for_reservation'
    - Returns a 400 Bad Request response if the listing is not available for the specified date range
    - Uses 'ReservationSerializer' for valid data and saves the reservation

--- src/reservation/views.py
+++ src/reservation/views.py
@@ -1,10 +1,11 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from .models import Listing
-from .serializers import ListingSerializer
+from .serializers import ListingSerializer, ReservationSerializer
 from .utils import validate_input_dates, parse_dates, get_listings_in_date_range
 
 
@@ -37,3 +38,33 @@ def show_all_available_listing(request):
 
     serializer = ListingSerializer(available_listings, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def add_reservation(request):
+    listing_id, start_date, end_date = request.data.get('listing'), request.data.get('start_date'), request.data.get(
+        'end_date')
+
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    error_response = validate_input_dates(start_date, end_date)
+    if error_response:
+        return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
+
+    start_date, end_date = parse_dates(start_date, end_date)
+
+    if not is_listing_available_for_reservation(listing, start_date, end_date):
+        return Response({'error': f'Listing not available for reservation until {end_date}.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    serializer = ReservationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def is_listing_available_for_reservation(listing, start_date, end_date):
+    available_listings = get_listings_in_date_range(start_date, end_date)
+    return listing.id in available_listings.values_list('id', flat=True)
