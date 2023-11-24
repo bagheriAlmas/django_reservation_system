53eaa27b449a5138e1671a42710a154e25690ac0
mahdibagheri <bagheri.almas@gmail.com>
2023-11-24 21:37:03 +0330

    Create URL pattern for 'add_reservation' view

--- src/reservation/urls.py
+++ src/reservation/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import show_all_listings, show_all_available_listing
+from .views import show_all_listings, show_all_available_listing, add_reservation
 
 urlpatterns = [
     path('listings/', show_all_listings, name='listing-list'),
     path('available_listings/', show_all_available_listing, name='available_listing-list'),
+    path('add_reservation/', add_reservation, name='add_reservation'),
 ]
