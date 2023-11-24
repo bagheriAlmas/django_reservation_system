89ffb3682cd0599d7862a6ba7209fdd0aadf9af9
mahdibagheri <bagheri.almas@gmail.com>
2023-11-24 21:32:48 +0330

    Implement API view for available listings in a date range
    - 'show_all_available_listing' handles GET requests
    - Validates and parses input dates, returns 400 Bad Request on errors
    - Retrieves and serializes available listings in the specified date range

--- src/reservation/views.py
+++ src/reservation/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 
 from .models import Listing
 from .serializers import ListingSerializer
+from .utils import validate_input_dates, parse_dates, get_listings_in_date_range
 
 
 @api_view(['GET'])
@@ -20,3 +21,19 @@ def show_all_listings(request):
         return paginator.get_paginated_response(serializer.data)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['GET'])
+def show_all_available_listing(request):
+    start_date, end_date = request.data.get('start_date'), request.data.get('end_date')
+
+    error_response = validate_input_dates(start_date, end_date)
+    if error_response:
+        return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
+
+    start_date, end_date = parse_dates(start_date, end_date)
+
+    available_listings = get_listings_in_date_range(start_date, end_date)
+
+    serializer = ListingSerializer(available_listings, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
