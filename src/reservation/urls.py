dd3e629146a38bbd10378d263a822b1beb7b3bfc
mahdibagheri <bagheri.almas@gmail.com>
2023-11-24 21:34:37 +0330

    Create URL pattern for 'show_all_available_listing' view

--- src/reservation/urls.py
+++ src/reservation/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import show_all_listings
+from .views import show_all_listings, show_all_available_listing
 
 urlpatterns = [
     path('listings/', show_all_listings, name='listing-list'),
+    path('available_listings/', show_all_available_listing, name='available_listing-list'),
 ]
