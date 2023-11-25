fc6e738d7c0b6f8fb2c80069cb528e15be35bbb0
mahdibagheri <bagheri.almas@gmail.com>
2023-11-25 17:24:50 +0330

    Update urls names

--- src/reservation/urls.py
+++ src/reservation/urls.py
@@ -5,12 +5,12 @@ from .views.class_views import ShowAllListings, ShowAllAvailableListings, AddRes
 urlpatterns = [
     # Function Views
     path('v1/listings/', show_all_listings, name='listing-list'),
-    path('v1/available_listings/', show_all_available_listings, name='available_listing-list'),
-    path('v1/add_reservation/', add_reservation, name='add_reservation'),
-    path('v1/reports/', overview_reports, name='overview_reports'),
+    path('v1/available_listings/', show_all_available_listings, name='available-listing-list'),
+    path('v1/add_reservation/', add_reservation, name='add-reservation'),
+    path('v1/reports/', overview_reports, name='overview-reports'),
     #Class-Base Views
-    path('v2/listings/', ShowAllListings.as_view(), name='class_listing-list'),
-    path('v2/available_listings/', ShowAllAvailableListings.as_view(), name='class_available_listing-list'),
-    path('v2/add_reservation/', AddReservationView.as_view(), name='class_add_reservation'),
-    path('v2/reports/', OverviewReportsView.as_view(), name='class_overview_reports'),
+    path('v2/listings/', ShowAllListings.as_view(), name='class-listing-list'),
+    path('v2/available_listings/', ShowAllAvailableListings.as_view(), name='class-available-listing-list'),
+    path('v2/add_reservation/', AddReservationView.as_view(), name='class-add-reservation'),
+    path('v2/reports/', OverviewReportsView.as_view(), name='class-overview-reports'),
 ]
