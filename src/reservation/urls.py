3aca6936944412a8798a84a57c99f6e836981310
mahdibagheri <bagheri.almas@gmail.com>
2023-11-25 15:17:42 +0330

    Add class-base Views and separate from function views

--- src/reservation/urls.py
+++ src/reservation/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import show_all_listings, show_all_available_listings, add_reservation, overview_reports
-
+from reservation.views.function_views import show_all_listings, show_all_available_listings, add_reservation, overview_reports
+from .views.class_views import ShowAllListings, ShowAllAvailableListings, AddReservationView, OverviewReportsView
 urlpatterns = [
-    path('listings/', show_all_listings, name='listing-list'),
-    path('available_listings/', show_all_available_listings, name='available_listing-list'),
-    path('add_reservation/', add_reservation, name='add_reservation'),
-    path('reports/', overview_reports, name='overview_reports'),
+    # Function Views
+    path('v1/listings/', show_all_listings, name='listing-list'),
+    path('v1/available_listings/', show_all_available_listings, name='available_listing-list'),
+    path('v1/add_reservation/', add_reservation, name='add_reservation'),
+    path('v1/reports/', overview_reports, name='overview_reports'),
+    #Class-Base Views
+    path('v2/listings/', ShowAllListings.as_view(), name='class_listing-list'),
+    path('v2/available_listings/', ShowAllAvailableListings.as_view(), name='class_available_listing-list'),
+    path('v2/add_reservation/', AddReservationView.as_view(), name='class_add_reservation'),
+    path('v2/reports/', OverviewReportsView.as_view(), name='class_overview_reports'),
 ]
