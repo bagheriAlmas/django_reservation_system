1ade12b19cc3760d7c999d5583c5497da2a4d79b
mahdibagheri <bagheri.almas@gmail.com>
2023-11-27 10:48:54 +0330

    Update function_views.py - Separate swagger configs to another file. - Optimizing codes and removing duplicate codes

--- src/reservation/views/function_views.py
+++ src/reservation/views/function_views.py
@@ -1,120 +1,75 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
-from drf_yasg import openapi
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-
 from reservation.models import Listing
 from reservation.serializers import ListingSerializer, ReservationSerializer
-from reservation.utils import validate_input_dates, parse_dates, get_listings_in_date_range
+from reservation.swagger_decorators import available_listings_swagger_decorator, add_reservation_swagger_decorator
+from reservation.utils import validate_input_dates, available_listings_in_date_range_query
+
+
+def listing_serializers_paginate_response(request, queryset):
+    paginator = PageNumberPagination()
+    try:
+        paginated_listings = paginator.paginate_queryset(queryset, request)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    serializer = ListingSerializer(paginated_listings, many=True)
+    return paginator.get_paginated_response(serializer.data)
+
+
+def listing_paginated_items(request, queryset):
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 10)
+
+    try:
+        paginated_items = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_items = paginator.page(1)
+    except EmptyPage:
+        paginated_items = paginator.page(paginator.num_pages)
+    return paginated_items
+
 
 
 @api_view(['GET'])
 def show_all_listings(request):
-    try:
-        paginator = PageNumberPagination()
-
-        listings = Listing.objects.all()
-        paginated_listings = paginator.paginate_queryset(listings, request)
-
-        serializer = ListingSerializer(paginated_listings, many=True)
-        return paginator.get_paginated_response(serializer.data)
-    except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    listings = Listing.objects.all()
+    response = listing_serializers_paginate_response(request, listings)
+    return response
 
 
-@swagger_auto_schema(
-    method='get',
-    manual_parameters=[
-        openapi.Parameter(
-            name='start_date',
-            in_=openapi.IN_QUERY,
-            type=openapi.TYPE_STRING,
-            format=openapi.FORMAT_DATE,
-            description='Start date for availability search',
-        ),
-        openapi.Parameter(
-            name='end_date',
-            in_=openapi.IN_QUERY,
-            type=openapi.TYPE_STRING,
-            format=openapi.FORMAT_DATE,
-            description='End date for availability search',
-        ),
-    ],
-    responses={
-        200: openapi.Response(
-            description='OK',
-            schema=openapi.Schema(
-                type=openapi.TYPE_OBJECT,
-                properties={
-                    'count': openapi.Schema(type=openapi.TYPE_INTEGER),
-                    'next': openapi.Schema(type=openapi.TYPE_STRING),
-                    'previous': openapi.Schema(type=openapi.TYPE_STRING),
-                    'results': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_OBJECT)),
-                },
-            ),
-        ),
-        400: 'Bad Request',
-        500: 'Internal Server Error',
-    },
-)
+@available_listings_swagger_decorator
 @api_view(['GET'])
 def show_all_available_listings(request):
-    start_date, end_date = request.query_params.get('start_date'), request.query_params.get('end_date')
-
-    error_response = validate_input_dates(start_date, end_date)
-    if error_response:
-        return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
-
-    start_date, end_date = parse_dates(start_date, end_date)
-
     try:
-        paginator = PageNumberPagination()
+        start_date, end_date = validate_input_dates(request.query_params.get('start_date') or None, request.query_params.get('end_date') or None)
+    except ValidationError as e:
+        return Response({"error":e.detail})
 
-        available_listings = get_listings_in_date_range(start_date, end_date)
-
-        paginated_listing = paginator.paginate_queryset(available_listings, request)
-
-        serializer = ListingSerializer(paginated_listing, many=True)
-        return paginator.get_paginated_response(serializer.data)
-    except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    available_listings = available_listings_in_date_range_query(start_date, end_date)
+    response = listing_serializers_paginate_response(request, available_listings)
+    return response
 
 
-@swagger_auto_schema(
-    method='post',
-    request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            'listing': openapi.Schema(type=openapi.TYPE_INTEGER),
-            'start_date': openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_DATE),
-            'end_date': openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_DATE),
-        },
-        required=['listing', 'start_date', 'end_date'],
-    ),
-    responses={
-        201: 'Created',
-        400: 'Bad Request',
-    },
-)
+@add_reservation_swagger_decorator
 @api_view(['POST'])
 def add_reservation(request):
     listing_id, start_date, end_date = request.data.get('listing'), request.data.get('start_date'), request.data.get(
         'end_date')
-
     listing = get_object_or_404(Listing, pk=listing_id)
 
-    error_response = validate_input_dates(start_date, end_date)
-    if error_response:
-        return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        start_date, end_date = validate_input_dates(request.query_params.get('start_date') or None, request.query_params.get('end_date') or None)
+    except ValidationError as e:
+        return Response({"error":e.detail})
 
-    start_date, end_date = parse_dates(start_date, end_date)
-
-    if not is_listing_available_for_reservation(listing, start_date, end_date):
+    available_listings = available_listings_in_date_range_query(start_date, end_date)
+    if not listing.id in available_listings.values_list('id', flat=True):
         return Response({'error': f'Listing not available for reservation until {end_date}.'},
                         status=status.HTTP_400_BAD_REQUEST)
 
@@ -126,53 +81,22 @@ def add_reservation(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-def is_listing_available_for_reservation(listing, start_date, end_date):
-    available_listings = get_listings_in_date_range(start_date, end_date)
-    return listing.id in available_listings.values_list('id', flat=True)
-
-
 @api_view(['GET'])
 def overview_reports(request):
     listings = Listing.objects.all()
-    listings_per_page = 10
-    # Get the current page number from the request's GET parameters
-    page = request.GET.get('page', 1)
-
-    # Paginate the listings
-    paginator = Paginator(listings, listings_per_page)
-    try:
-        listings = paginator.page(page)
-    except PageNotAnInteger:
-        listings = paginator.page(1)
-    except EmptyPage:
-        listings = paginator.page(paginator.num_pages)
-    return render(request, 'pages/listings_report.html', {"listings": listings})
+    paginated_listings = listing_paginated_items(request, listings)
+    return render(request, 'pages/listings_report.html', {"listings": paginated_listings})
 
 
 @api_view(['GET'])
 def listing_details(request, pk):
     listings = Listing.objects.get(pk=pk)
-    listings_per_page = 10
 
-    # Get the page number from the request's GET parameters
-    page = request.GET.get('page', 1)
-
-    # Create a Paginator object
-    paginator = Paginator(listings.reservations.all(), listings_per_page)
-
-    try:
-        # Get the specified page
-        reservations_page = paginator.page(page)
-    except PageNotAnInteger:
-        # If the page parameter is not an integer, show the first page
-        reservations_page = paginator.page(1)
-    except EmptyPage:
-        # If the page is out of range, show the last page
-        reservations_page = paginator.page(paginator.num_pages)
+    reservations = listings.reservations.all()
+    paginated_reservations = listing_paginated_items(request, reservations)
 
     context = {
         'listings': listings,
-        'reservations_page': reservations_page,
+        'reservations_page': paginated_reservations,
     }
-
     return render(request, 'pages/listing_details.html', context)
