6f5ed7f24a523db7cb5b7a33b930f7903077c9f9
mahdibagheri <bagheri.almas@gmail.com>
2023-11-27 11:48:02 +0330

    Adding some features to the admin panel for Listing model and Reservations model

--- src/reservation/admin.py
+++ src/reservation/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import Listing,Reservation
+from .models import Listing, Reservation
 
-admin.site.register(Listing)
-admin.site.register(Reservation)
+
+class ReservationInline(admin.TabularInline):
+    model = Reservation
+    extra = 1
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'address', 'description')
+    search_fields = ('name', 'owner__username', 'address')
+    list_filter = ('name', 'address')
+    inlines = [ReservationInline, ]
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'listing', 'start_date', 'end_date', 'duration')
+    search_fields = ('name', 'listing__name', 'listing__owner__username')
+    list_filter = ('start_date', 'end_date')
+    date_hierarchy = 'start_date'
+
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Reservation, ReservationAdmin)
