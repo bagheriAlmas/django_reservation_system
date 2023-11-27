223c73f782abcda8e082489ddb6092f25cf33a45
mahdibagheri <bagheri.almas@gmail.com>
2023-11-27 11:27:46 +0330

    Change get_difference_date_days to duration to make it more understandable

--- src/reservation/models.py
+++ src/reservation/models.py
@@ -27,6 +27,7 @@ class Reservation(models.Model):
         if self.start_date and self.end_date and self.start_date > self.end_date:
             raise ValidationError("End time must be greater than start time")
 
-    def get_difference_date_days(self):
+    @property
+    def duration(self):
         difference = self.end_date - self.start_date
         return difference.days + 1
