422ed20258f4e753d4fc8bfbef7b3fdefab9c1d8
mahdibagheri <bagheri.almas@gmail.com>
2023-11-27 10:41:54 +0330

    Optimizing Utils codes,merge some functions and remove unnecessary functions

--- src/reservation/utils.py
+++ src/reservation/utils.py
@@ -1,51 +1,35 @@
-from datetime import datetime
 from django.db.models import Q
+from datetime import datetime
+
+from rest_framework.exceptions import ValidationError
 
 from .models import Listing
 
 
-def is_valid_date_format(date_string):
-    print(date_string)
-    print(type(date_string))
-    try:
-        datetime.strptime(str(date_string), '%Y-%m-%d').date()
-        return True
-    except ValueError:
-        return False
-
-
-def get_listings_in_date_range(start_date, end_date):
+def available_listings_in_date_range_query(start_date, end_date):
     unavailable_listings = Listing.objects.filter(
         Q(reservations__start_date__lte=start_date, reservations__end_date__gte=start_date) |
         Q(reservations__start_date__lte=end_date, reservations__end_date__gte=end_date) |
         Q(reservations__start_date__gte=start_date, reservations__end_date__lte=end_date)
     )
+
     available_listings = Listing.objects.exclude(id__in=unavailable_listings.values_list('id', flat=True))
     return available_listings
 
 
+
 def validate_input_dates(start_date, end_date):
-    start_date, end_date = parse_dates(start_date, end_date)
     if start_date is None or end_date is None:
-        return {'start_date': 'is required and must be in the format YYYY-MM-DD.',
-                'end_date': 'is required and must be in the format YYYY-MM-DD.'}
+        raise ValidationError('Both start_date and end_date are required.')
+
+    start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+    end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
 
     if start_date < datetime.today().date() or end_date < datetime.today().date():
-        return {'date error': 'The selected day must not be past date.', }
+        raise ValidationError('The selected day must not be a past date.')
 
     if start_date > end_date:
-        return {'error': 'Start date must be before end date.'}
-
-    if not is_valid_date_format(start_date) or not is_valid_date_format(end_date):
-        return {'error': 'Invalid date format. Please use the format "YYYY-MM-DD".'}
-
-    return None
+        raise ValidationError('Start date must be before end date.')
+    return start_date, end_date
 
 
-def parse_dates(start_date, end_date):
-    try:
-        start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
-        end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-        return start_date, end_date
-    except ValueError as e:
-        return None, None
