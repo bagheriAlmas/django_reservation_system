a04e49cd908c2cd6057f0905b336f083e6439bf3
mahdibagheri <bagheri.almas@gmail.com>
2023-11-27 16:43:41 +0330

    Regenerate class_views.py codes

--- src/reservation/views/class_views.py
+++ src/reservation/views/class_views.py
@@ -1,121 +1,102 @@
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+import logging
+from django.core.cache import cache
 from django.shortcuts import render
-from django.views import View
-from django.views.generic import ListView
-from rest_framework import generics, mixins, pagination, serializers
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
 from rest_framework import status
-from ..models import Listing
-from ..serializers import ListingSerializer, ReservationSerializer
-from ..utils import validate_input_dates, parse_dates, get_listings_in_date_range
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import ListAPIView, CreateAPIView, get_object_or_404
+from rest_framework.response import Response
+from reservation.models import Listing
+from reservation.serializers import ReservationSerializer, ListingSerializer
+from reservation.swagger_decorators import available_listings_swagger_decorator, add_reservation_swagger_decorator
+from reservation.utils import (parse_input_dates, available_listings_in_date_range_query,
+                               listing_serializers_paginate_response, listing_paginated_items)
 
+logger = logging.getLogger(__name__)
 
-class ShowAllListings(mixins.ListModelMixin, generics.GenericAPIView):
-    queryset = Listing.objects.all()
+class ShowAllListingsView(ListAPIView):
+    queryset = Listing.objects.all().select_related('owner')
     serializer_class = ListingSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    def list(self, request, *args, **kwargs):
+        response = listing_serializers_paginate_response(request, self.get_queryset())
+        logger.info('ShowAllListingsView executed successfully')
+        return response
 
-
-class ShowAllAvailableListings(mixins.ListModelMixin, generics.GenericAPIView):
+class ShowAllAvailableListingsView(ListAPIView):
     serializer_class = ListingSerializer
 
     def get_queryset(self):
-        start_date = self.request.query_params.get('start_date')
-        end_date = self.request.query_params.get('end_date')
+        try:
+            start_date, end_date = parse_input_dates(self.request.query_params.get('start_date') or None,
+                                                     self.request.query_params.get('end_date') or None)
+        except ValidationError as e:
+            return Response({"error": e.detail}, status=status.HTTP_400_BAD_REQUEST)
 
-        error_response = validate_input_dates(start_date, end_date)
-        if error_response:
-            raise serializers.ValidationError(detail=error_response)
-        start_date, end_date = parse_dates(start_date, end_date)
-        return get_listings_in_date_range(start_date, end_date)
+        return available_listings_in_date_range_query(start_date, end_date)
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    def list(self, request, *args, **kwargs):
+        response = listing_serializers_paginate_response(request, self.get_queryset())
+        logger.info('ShowAllAvailableListingsView executed successfully')
+        return response
 
-
-class AddReservationView(mixins.CreateModelMixin, generics.GenericAPIView):
-    queryset = Listing.objects.all()
+class AddReservationView(CreateAPIView):
     serializer_class = ReservationSerializer
 
-    def post(self, request, *args, **kwargs):
-        listing_id, start_date, end_date = request.data.get('listing'), request.data.get(
-            'start_date'), request.data.get('end_date')
-
+    def create(self, request, *args, **kwargs):
+        listing_id = request.data.get('listing')
         listing = get_object_or_404(Listing, pk=listing_id)
 
-        error_response = validate_input_dates(start_date, end_date)
-        if error_response:
-            return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
-
-        start_date, end_date = parse_dates(start_date, end_date)
-
-        if not is_listing_available_for_reservation(listing, start_date, end_date):
-            return Response({'error': f'Listing not available for reservation until {end_date}.'},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        return self.create(request, *args, **kwargs)
-
-
-def is_listing_available_for_reservation(listing, start_date, end_date):
-    available_listings = get_listings_in_date_range(start_date, end_date)
-    return listing.id in available_listings.values_list('id', flat=True)
-
-
-class OverviewReportsView(View):
-    template_name = 'pages/listings_report.html'
-    items_per_page = 10
-
-    def get_context_data(self, page):
-        queryset = Listing.objects.all().select_related("owner")
-        paginator = Paginator(queryset, self.items_per_page)
-
         try:
-            listings = paginator.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            listings = paginator.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-            listings = paginator.page(paginator.num_pages)
+            start_date, end_date = parse_input_dates(request.data.get('start_date') or None,
+                                                     request.data.get('end_date') or None)
+        except ValidationError as e:
+            logger.info('Input dates have incorrect format')
+            return Response({"error": e.detail}, status=status.HTTP_400_BAD_REQUEST)
 
-        return {'listings': listings}
+        available_listings = available_listings_in_date_range_query(start_date, end_date)
+        if listing.id not in available_listings.values_list('id', flat=True):
+            logger.info('Failed to Reservation')
+            return Response({'error': f'Listing not available for reservation until {end_date}.'},
+                            status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            cache.delete(listing_id)
+            logger.info('Reserved successfully')
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class OverviewReportsView(ListAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+    def list(self, request, *args, **kwargs):
+        paginated_listings = listing_paginated_items(request, self.get_queryset())
+        logger.info('OverviewReportsView executed successfully')
+        return render(request, 'pages/listings_report.html', {"listings": paginated_listings})
+
+class ListingDetailsView(ListAPIView):
+    serializer_class = ListingSerializer
 
     def get(self, request, *args, **kwargs):
-        page = request.GET.get('page', 1)
-        context = self.get_context_data(page)
-        return render(request, self.template_name, context)
+        pk = self.kwargs.get('pk')
+        if cache.get(pk):
+            listing = cache.get(pk)
+            logger.info('Load data from cache')
+        else:
+            listing = get_object_or_404(Listing, pk=pk)
+            cache.set(pk, listing)
+            logger.info('Load data from db')
 
+        reservations = listing.reservations.all()
+        paginated_reservations = listing_paginated_items(request, reservations)
 
-class ListingDetailsView(ListView):
-    model = Listing
-    template_name = 'pages/listing_details.html'
-    context_object_name = 'listings'
-    paginate_by = 10
+        context = {
+            'listings': listing,
+            'reservations_page': paginated_reservations,
+        }
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        # Get the page number from the request's GET parameters
-        page = self.request.GET.get('page', 1)
-
-        # Create a Paginator object
-        paginator = Paginator(context['listings'].reservations.all(), self.paginate_by)
-
-        try:
-            # Get the specified page
-            reservations_page = paginator.page(page)
-        except PageNotAnInteger:
-            # If the page parameter is not an integer, show the first page
-            reservations_page = paginator.page(1)
-        except EmptyPage:
-            # If the page is out of range, show the last page
-            reservations_page = paginator.page(paginator.num_pages)
-
-        context['reservations_page'] = reservations_page
-        return context
-
-
-listing_details_view = ListingDetailsView.as_view()
+        logger.info('ListingDetailsView executed successfully')
+        return render(request, 'pages/listing_details.html', context)
