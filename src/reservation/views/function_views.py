ce196eb1a25f84ad2996c909c8985a8f0d03d966
mahdibagheri <bagheri.almas@gmail.com>
2023-11-27 12:45:24 +0330

    Remove cache with listing id which is insert new reservation

--- src/reservation/views/function_views.py
+++ src/reservation/views/function_views.py
@@ -53,6 +53,7 @@ def add_reservation(request):
     serializer = ReservationSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+        cache.delete(listing_id)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
